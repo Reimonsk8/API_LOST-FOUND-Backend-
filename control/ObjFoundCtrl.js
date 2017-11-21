77c6a46a4cc591393686f3d20e243f1d038298e8
Jose Ramón gomez armenta <Reimonsk8@gmail.com>
2017-11-21 15:36:05 -0800

    Update ObjFoundCtrl.js

--- control/ObjFoundCtrl.js
+++ control/ObjFoundCtrl.js
@@ -21,6 +21,8 @@ exports.addFoundObj=function(req,res,next){
 	console.log('POST/ObjFound');
 	var itemfound = new itemFound({
 
+		lat: req.body.lat,
+		lng: req.body.lng,
 		place: req.body.place,
 		date: req.body.date,
 		type: req.body.type,
@@ -38,9 +40,10 @@ exports.addFoundObj=function(req,res,next){
 exports.getFoundByFilter=function(req, res, next){
 	console.log('GET/ObjFound/:filter');	
 	console.log(req.params.filter);
-	itemFound.find({ $or: [ { place: req.params.filter },{ date: req.params.filter },{ type: req.params.filter },
-		{ contact: req.params.filter },{ description: req.params.filter } ] } ,
-		function(err,ObjFoundTable){
+	itemFound.find({ $or: [ 
+		{ place: req.params.filter },{ date: req.params.filter },{ type: req.params.filter },
+		{ contact: req.params.filter },{ description: req.params.filter } 
+		]},function(err,ObjFoundTable){
   		if(err){
   			res.send(500, err.message);
   		}else{
@@ -50,25 +53,12 @@ exports.getFoundByFilter=function(req, res, next){
   	});
 };
 
-
-exports.getFoundById=function(req, res, next){
-	console.log('GET/ObjFound/:id');	
-	console.log(req.params.id);
-	itemFound.find({ _id: req.params.id },function(err,ObjFoundTable){
-  		if(err){
-  			res.send(500, err.message);
-  		}else{
-  			console.log('GET/ObjFound');
-        	res.status(200).send(ObjFoundTable);
-  		}
-  	});
-};
 
 
 exports.deleteFoundObj=function(req, res, next){
-	console.log('DELETE/ObjFound/:id');
-	console.log(req.params.id);
-	itemFound.remove({_id: req.params.id},function(err,ObjFoundTable){
+	console.log('DELETE/ObjFound/:filter');
+	console.log(req.params.filter);
+	itemFound.remove({_id: req.params.filter},function(err,ObjFoundTable){
   		if(err){
   			res.send(500, err.message);
   		}else{
@@ -80,17 +70,18 @@ exports.deleteFoundObj=function(req, res, next){
 
 
 exports.updateFoundObj=function(req, res, next){
-	console.log('PUT/ObjFound/:id');
-	console.log(req.params.id);
+	console.log('PUT/ObjFound/:filter');
+	console.log(req.params.filter);
 	console.log(req.body);
-	itemFound.update({_id: req.params.id},
-				{$set:{	place:req.body.place, date:req.body.date, type:req.body.type, contact:req.body.contact,
-				description:req.body.description}},
+	itemFound.update({_id: req.params.filter},
+				{$set:{	
+					lat:req.body.lat, lng:req.body.lng, place:req.body.place, date:req.body.date, 
+					type:req.body.type, contact:req.body.contact,description:req.body.description}},
 				function(err,ObjFoundTable){
 		if(err){
 			res.send(500,err.message);
 		}else{
-			itemFound.find({_id: req.params.id},function(err,ObjFoundTable){
+			itemFound.find({_id: req.params.filter},function(err,ObjFoundTable){
 	  			if(err){
 		  			res.send(500, err.message);
 		  		}else{
